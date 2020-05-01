9aa9b8aa74235ad7e9c61156fe2f14eddbb8948e
arirang372 <hyonoo419@gmail.com>
2020-04-30 22:35:38 -0400

    removed the unnecessary logging

--- networklib-livedata/src/main/java/com/john/networklib_livedata/receivers/GPSStatusChangeReceiver.java
+++ networklib-livedata/src/main/java/com/john/networklib_livedata/receivers/GPSStatusChangeReceiver.java
@@ -3,7 +3,6 @@ package com.john.networklib_livedata.receivers;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
-import android.widget.Toast;
 
 import com.john.networklib_livedata.GPSStatus;
 import com.john.networklib_livedata.events.GpsLiveEvent;
@@ -14,44 +13,43 @@ import com.john.networklib_livedata.logger.NetLogger;
  */
 public class GPSStatusChangeReceiver extends BaseBroadcastReceiver {
 
-	private static final String EVENT_NAME = GPSStatusChangeReceiver.class.getSimpleName();
-	public final static String INTENT = "android.location.PROVIDERS_CHANGED";
-	public final static String INTENT_EXTRA = "networkevents.intent.extra.CONNECTED_TO_GPS";
-	private final GpsLiveEvent event;
-	private GPSStatus gpsStatus = GPSStatus.GPS_OFF;
+    private static final String EVENT_NAME = GPSStatusChangeReceiver.class.getSimpleName();
+    public final static String INTENT = "android.location.PROVIDERS_CHANGED";
+    public final static String INTENT_EXTRA = "networkevents.intent.extra.CONNECTED_TO_GPS";
+    private final GpsLiveEvent event;
+    private GPSStatus gpsStatus = GPSStatus.GPS_OFF;
 
-	public GPSStatusChangeReceiver(GpsLiveEvent event, NetLogger netLogger, Context context) {
-		super(netLogger, context);
-		this.event = event;
-		addEvent(EVENT_NAME, event);
-	}
+    public GPSStatusChangeReceiver(GpsLiveEvent event, NetLogger netLogger, Context context) {
+        super(netLogger, context);
+        this.event = event;
+        addEvent(EVENT_NAME, event);
+    }
 
-	public GPSStatus getGPSStatus() {
-		return this.gpsStatus;
-	}
+    public GPSStatus getGPSStatus() {
+        return this.gpsStatus;
+    }
 
-	private boolean isGPSOn(Context context) {
-		final LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-		return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-	}
+    private boolean isGPSOn(Context context) {
+        final LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		Toast.makeText(context, "GPS Status has changed ...", Toast.LENGTH_LONG).show();
-		processGpsEvent();
-	}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        processGpsEvent();
+    }
 
-	public void processGpsEvent() {
-		boolean isGpsOn = isGPSOn(context);
-		if (isGpsOn) {
-			this.gpsStatus = GPSStatus.GPS_ON;
-		} else {
-			this.gpsStatus = GPSStatus.GPS_OFF;
-		}
-		postFromAnyThread(event, isGpsOn);
-	}
+    public void processGpsEvent() {
+        boolean isGpsOn = isGPSOn(context);
+        if (isGpsOn) {
+            this.gpsStatus = GPSStatus.GPS_ON;
+        } else {
+            this.gpsStatus = GPSStatus.GPS_OFF;
+        }
+        postFromAnyThread(event, isGpsOn);
+    }
 
-	public void setGPSStatus(GPSStatus status) {
-		this.gpsStatus = status;
-	}
+    public void setGPSStatus(GPSStatus status) {
+        this.gpsStatus = status;
+    }
 }
